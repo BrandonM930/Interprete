b5b7a8c8f243941a0774fd0675c4976ce453adf7
Brandon Mendoza Niño <118572820+BrandonM930@users.noreply.github.com>
2023-11-19 23:54:24 -0500

    Create interface.py
    Añadiendo el código de la interface, con traducción y ayuda

---
+++ interface.py
@@ -0,0 +1,176 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import scrolledtext
+from googletrans import Translator
+import threading
+import pyttsx3
+import ply.yacc as yacc
+from lexer import lexer
+
+# Resto de los imports...
+
+class SimpleInterpreterGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Simple Interpreter GUI")
+
+        self.translator = Translator()
+        self.current_language = "en"
+        self.languages = {"en": "English", "es": "Spanish", "fr": "French"}
+
+        self.language_var = tk.StringVar()
+        self.language_var.set("en")
+
+        self.language_label = ttk.Label(master, text="Select a language:")
+        self.language_label.pack()
+
+        self.language_selector = ttk.Combobox(master, values=list(self.languages.values()), state="readonly",
+                                              textvariable=self.language_var)
+        self.language_selector.current(0)
+        self.language_selector.pack()
+        self.language_selector.bind("<<ComboboxSelected>>", self.change_language)
+
+        self.editor_label = tk.Label(master, text="Enter your code to validate with the interpreter:")
+        self.editor_label.pack(pady=5)
+
+        self.editor = scrolledtext.ScrolledText(master, width=40, height=10)
+        self.editor.pack(pady=5)
+
+        self.run_button = tk.Button(master, text="Run", command=self.run_code)
+        self.run_button.pack(side=tk.LEFT, padx=5)
+
+        self.validate_button = tk.Button(master, text="Validate", command=self.validate_code)
+        self.validate_button.pack(side=tk.RIGHT, padx=5)
+
+        self.help_button = tk.Button(master, text="Help", command=self.show_help)
+        self.help_button.pack(side=tk.RIGHT, padx=5)
+
+        self.result_text = tk.Text(master, height=2, width=40)
+        self.result_text.pack(pady=5)
+
+        self.example_labels = []
+        self.example_buttons = []
+        self.examples_frame = tk.Frame(master)
+        self.examples_frame.pack(pady=5)
+
+        # Show the initial instruction
+        self.show_result("Enter your code to validate with the interpreter.")
+
+    def run_code(self):
+        code = self.editor.get("1.0", tk.END)
+        translated_code = self.translate_code(code)
+        try:
+            parser_result = parser.parser.parse(translated_code, lexer=lexer)
+            result_str = f"Parser result: {parser_result}"
+            self.show_result(result_str)
+        except Exception as e:
+            error_str = f"Error during parsing/execution: {e}"
+            self.show_result(error_str)
+
+    def validate_code(self):
+        code = self.editor.get("1.0", tk.END)
+        translated_code = self.translate_code(code)
+        try:
+            parser.parser.parse(translated_code, lexer=lexer)
+            self.show_result("The code is valid.")
+        except Exception as e:
+            error_str = f"Syntax error: {e}"
+            self.show_result(error_str)
+
+    def translate_code(self, code):
+        target_language = self.language_var.get()
+        translation = self.translator.translate(code, dest=target_language)
+        return translation.text
+
+    def show_result(self, result):
+        # Clear previous content before showing the new result
+        self.result_text.delete(1.0, tk.END)
+        self.result_text.insert(tk.END, result)
+
+    def show_help(self):
+        help_window = tk.Toplevel(self.master)
+        help_window.title("Help")
+
+        help_text = """
+        This is a simple interpreter GUI that allows you to enter and validate code snippets.
+        
+        Features:
+        - Select a language from the dropdown to translate the code.
+        - Enter code in the editor and click "Run" or "Validate."
+        - The result will be displayed in the result area.
+        - Click "Help" to view examples and usage instructions.
+        
+        Examples:
+        - Basic arithmetic: x = 10 + y * 5
+        - Conditionals: if x > 0: positive = True else: positive = False
+        - Loops: for i in range(5): print(i)
+        - Functions: def multiply(a, b): return a * b
+        
+        Enjoy coding!
+        """
+
+        help_label = tk.Label(help_window, text=help_text, justify=tk.LEFT)
+        help_label.pack(padx=10, pady=10)
+
+    def show_examples(self):
+        examples = [
+            "x = 10 + y * 5",
+            "result = 2 * (3 + 4)",
+            "a = 5 * (10 - b)",
+            "c = 8 / (2 * d)",
+            "x = 10 * y + z",
+            "invalid = 3 + * 5",
+            "",
+            "# Conditional Example",
+            "if x > 0:",
+            "    positive = True",
+            "else:",
+            "    positive = False",
+            "",
+            "# Loop Example",
+            "for i in range(5):",
+            "    print(i)",
+            "",
+            "# Function Example",
+            "def multiply(a, b):",
+            "    return a * b",
+            "",
+            "result = multiply(3, 4)",
+        ]
+
+        # Clear the frame of previous labels and buttons
+        for label, button in zip(self.example_labels, self.example_buttons):
+            label.destroy()
+            button.destroy()
+        self.example_labels = []
+        self.example_buttons = []
+
+        # Show examples in the frame and create buttons
+        for i, example in enumerate(examples):
+            label = tk.Label(self.examples_frame, text=f"Example {i + 1}: {example}")
+            label.grid(row=i, column=0, sticky=tk.W, padx=5)
+            self.example_labels.append(label)
+            button = tk.Button(self.examples_frame, text=f"Use Example {i + 1}", command=lambda ex=example: self.use_example(ex))
+            button.grid(row=i, column=1, padx=5)
+            self.example_buttons.append(button)
+
+    def use_example(self, example):
+        # Clear the editor before adding the example
+        self.editor.delete("1.0", tk.END)
+        # Add the example to the editor
+        self.editor.insert(tk.END, example)
+        # Clear the previous result
+        self.show_result("")
+
+    def change_language(self, event):
+        # Function to change the language when a new one is selected
+        self.current_language = next(key for key, value in self.languages.items() if value == self.language_selector.get())
+        print("Language changed to:", self.current_language)
+
+def main():
+    root = tk.Tk()
+    app = SimpleInterpreterGUI(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
