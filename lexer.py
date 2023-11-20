7bfb74db38be684e24fc8605eb1ffe75793e633e
Brandon Mendoza Niño <118572820+BrandonM930@users.noreply.github.com>
2023-11-19 23:55:21 -0500

    Create lexer.py
    Código del lexer

---
+++ lexer.py
@@ -0,0 +1,58 @@
+import ply.lex as lex
+
+# Lista de tokens
+tokens = (
+    'ID',
+    'EQUALS',
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
+    'IF',
+    'ELSE',
+    'WHILE',
+    'DO',
+    'FOR',
+    'FUNCTION',
+    'COMMA',
+)
+
+# Definición de patrones de expresiones regulares
+t_EQUALS = r'='
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_COMMA = r','
+t_IF = r'if'
+t_ELSE = r'else'
+t_WHILE = r'while'
+t_DO = r'do'
+t_FOR = r'for'
+t_FUNCTION = r'function'
+
+# Ignorar espacios y tabulaciones
+t_ignore = ' \t'
+
+# Expresiones regulares para tokens más complejos
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    return t
+
+def t_NUMBER(t):
+    r'\d+(\.\d+)?'
+    t.value = float(t.value)
+    return t
+
+# Manejo de errores
+def t_error(t):
+    print(f"Illegal character '{t.value[0]}'")
+    t.lexer.skip(1)
+
+# Crear el lexer
+lexer = lex.lex()
